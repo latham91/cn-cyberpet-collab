a9718340203aa97488fe595e8fae4db6b6f6a956
Aaron Latham <latham91@icloud.com>
2024-01-17 18:54:26 +0000

    Updated character classes

--- script.js
+++ script.js
@@ -6,33 +6,60 @@ class Character {
         this.thirst = 100;
         this.boredom = 100;
         this.health = 100;
+        this.tiredness = 100;
         this.mood = mood;
     }
 
     // Methods for the character class
+    // (refill oxygen, eat, drink, exercise, sleep)
+    // TODO: Will adjust numbers later
+    refillOxygen() {
+        this.oxygen += 25;
+        this.tiredness -= 25;
+    }
+
+    eat() {
+        this.hunger += 25;
+        this.thirst -= 25;
+        this.tiredness -= 25;
+    }
+
+    drink() {
+        this.thirst += 25;
+        this.hunger -= 25;
+        this.tiredness -= 25;
+    }
+
+    exercise() {
+        this.tiredness -= 25;
+        this.hunger -= 25;
+        this.thirst -= 25;
+        this.boredom += 25;
+    }
+
+    sleep() {
+        this.tiredness += 25;
+        this.hunger -= 25;
+        this.thirst -= 25;
+        this.boredom -= 25;
+    }
 }
 
 // Constructing the 3 different playable characters
 class Aaron extends Character {
-    constructor(mood) {
-        super(mood);
-    }
+    constructor() {}
 
     // Unique methods?
 }
 
 class Constantin extends Character {
-    constructor(mood) {
-        super(mood);
-    }
+    constructor() {}
 
     // Unique methods?
 }
 
 class Reuben extends Character {
-    constructor(health, mood) {
-        super(health, mood);
-    }
+    constructor() {}
 
     // Unique methods?
 }
