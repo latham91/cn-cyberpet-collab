2cdeaf5571efb4120a8db50b8f930481e699f0a9
Aaron Latham <latham91@icloud.com>
2024-01-18 14:50:49 +0000

    Created basic game loop

--- script.js
+++ script.js
@@ -46,6 +46,10 @@ class Character {
         this.stats.thirst -= 25;
         this.stats.boredom -= 25;
     }
+
+    mood(mood) {
+        this.mood = mood;
+    }
 }
 
 // Constructing the 3 different playable characters
@@ -71,4 +75,85 @@ class Reuben extends Character {
     }
 
     // Unique methods?
+}
+
+// Variables
+let character;
+let isPlaying = false;
+
+// Function to create a character. example: createCharacter("Aaron", "happy");
+const createCharacter = (character, baseMood) => {
+    if (character === "Aaron") {
+        character = new Aaron(baseMood);
+    }
+
+    if (character === "Constantin") {
+        character = new Constantin(baseMood);
+    }
+
+    if (character === "Reuben") {
+        character = new Reuben(baseMood);
+    }
+};
+
+while (isPlaying) {
+    // When characters health reaches 0, game over.
+    if (character.stats.health <= 0) {
+        isPlaying = false;
+
+        // TODO: Add function to display game over screen
+    }
+
+    // Stat ticks every second
+    setInterval(() => {
+        // Base stat deduction. (Mood = "happy")
+        if (mood === "happy") {
+            character.stats.health -= 2;
+            character.stats.oxygen -= 2;
+            character.stats.hunger -= 2;
+            character.stats.thirst -= 2;
+            character.stats.energy -= 2;
+            character.stats.boredom -= 2;
+        }
+
+        // Tired stat deduction. (Mood = "tired")
+        if (mood === "tired") {
+            character.stats.health -= 4;
+            character.stats.oxygen -= 4;
+            character.stats.hunger -= 2;
+            character.stats.thirst -= 4;
+            character.stats.energy -= 6;
+            character.stats.boredom -= 2;
+        }
+
+        // Hungry stat deduction. (Mood = "hungry")
+        if (mood === "hungry") {
+            character.stats.health -= 4;
+            character.stats.oxygen -= 2;
+            character.stats.hunger -= 6;
+            character.stats.thirst -= 2;
+            character.stats.energy -= 4;
+            character.stats.boredom -= 2;
+        }
+
+        // Thirsty stat deduction. (Mood = "thirsty")
+        if (mood === "thirsty") {
+            character.stats.health -= 4;
+            character.stats.oxygen -= 2;
+            character.stats.hunger -= 2;
+            character.stats.thirst -= 6;
+            character.stats.energy -= 4;
+            character.stats.boredom -= 2;
+        }
+
+        // Bored stat deduction. (Mood = "bored")
+        if (mood === "bored") {
+            character.stats.health -= 4;
+            character.stats.oxygen -= 2;
+            character.stats.hunger -= 2;
+            character.stats.thirst -= 2;
+            character.stats.energy -= 2;
+            character.stats.boredom -= 6;
+        }
+    }, 1000);
 }
